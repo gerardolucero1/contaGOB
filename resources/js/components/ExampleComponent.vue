4dad6e21ddfbdcfbc1562f366768aab78fc75c77
Gerardo Lucero <gera_conecta@hotmail.com>
2019-12-02 19:40:31 -0700

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-app id="app">
+        <v-data-table
+            :headers="headers"
+            :items="desserts"
+            class="elevation-1"
+        ></v-data-table>
+    </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+        ],
+      }
+    },
+  }
+</script>
